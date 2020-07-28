9f09170694927eec7c6c347e5efca61181a64bae
rakin016 <rakin016@gmail.com>
2020-07-28 23:52:06 +0600

    T2M2F2

--- company_app.js
+++ company_app.js
@@ -2,6 +2,8 @@ var express 			= require('express');
 var bodyParser 			= require('body-parser');
 var login 				= require('./controller/login');
 var admin 				= require('./controller/admin');
+var addEmployee 		= require('./controller/addEmployee');
+
 var company_app 		= express();
 
 company_app.set('view engine', 'ejs');
@@ -10,6 +12,9 @@ company_app.set('view engine', 'ejs');
 company_app.use(bodyParser());
 company_app.use('/login', login);
 company_app.use('/admin', admin);
+company_app.use('/addEmployee', addEmployee);
+
+
 
 company_app.get('/', function(req, res){
 	res.send("This is Home page!<br> <a href='/login'>Click here to login</a> ");
